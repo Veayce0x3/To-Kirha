1691ae77be2fae891585b98adc2c5825bf4cefab
Veayce0x3 <Veayce0x3@users.noreply.github.com>
2026-03-19 19:11:36 +0000

    deploy: 83d9d1ddb75238e87b58168e9c978aede332a969

--- assets/chevron-right-CSHBo_db.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-D5Am7ZGe.js";import"./index-Du-4zmyU.js";import"./index.es-SBIGusQ0.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronRightSvg};
