3e2ea10b68a1aa9f2301687a654169dc5d933dd2
Veayce0x3 <Veayce0x3@users.noreply.github.com>
2026-03-20 22:43:24 +0000

    deploy: fe79b3ad85dfdc57fb6f72d31067c42b1a5ab6c8

--- assets/cursor-C26siV2S.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-PBYrcZL5.js";import"./index-scDP9GI0.js";import"./index.es-BrsG9et7.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
