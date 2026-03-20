654baaf8ae505be9c5d5518af2cbce2c4cc8e167
Veayce0x3 <Veayce0x3@users.noreply.github.com>
2026-03-20 13:54:14 +0000

    deploy: 1882e6f42c90cacf50a85aca52620f34537de112

---
+++ assets/chevron-left-CVv6MfY2.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-CwsNSwXp.js";import"./index-BJKrUdm5.js";import"./index.es-If4KLDIz.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronLeftSvg};
