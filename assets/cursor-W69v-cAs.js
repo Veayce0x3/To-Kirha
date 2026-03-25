198648840eca1dfab56e53c6d87e8dbc36fa8327
Veayce0x3 <Veayce0x3@users.noreply.github.com>
2026-03-25 07:36:46 +0000

    deploy: aff12edc7e090ddfa5e8cda7c57b019aff697068

--- assets/cursor-W69v-cAs.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-Bz_CSWnv.js";import"./index-hlOXJazv.js";import"./index.es-CgaBDR4s.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
