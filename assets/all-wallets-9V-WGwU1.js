56076463bb67ef46b5f9573d1d626133b363a21a
Veayce0x3 <Veayce0x3@users.noreply.github.com>
2026-05-02 12:41:19 +0000

    deploy: 3e185760a4fb0f88aef441680dad858fc407ac40

--- assets/all-wallets-9V-WGwU1.js
+++
@@ -1,6 +0,0 @@
-import{w as o}from"./core-DMFMcYRo.js";import"./wallet-vendor-CPMxfkUE.js";import"./react-vendor-3RdGqFZN.js";import"./index.es-Bl-P9cMy.js";import"./events-DQ172AOg.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
